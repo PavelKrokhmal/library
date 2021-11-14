dd4bd7f3176e0b88e9bf400eed04e186cec5346a
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-11-14 11:57:51 +0200

    added checkin & checkout methods to book model

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -11,13 +12,44 @@ class Book extends Model
 
     protected $guarded = [];
 
-    public function path() {
+    public function path()
+    {
         return '/books/' . $this->id;
     }
 
-    public function setAuthorIdAttribute($author) {
+    public function setAuthorIdAttribute($author)
+    {
         $this->attributes['author_id'] = (Author::firstOrCreate([
             'name' => $author
         ]))->id;
+    }
+
+    public function checkout(User $user)
+    {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now()
+        ]);
+    }
+
+    public function checkin(User $user)
+    {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+            ->whereNotNull('checked_out_at')
+            ->whereNull('checked_in_at')
+            ->first();
+
+        if (is_null($reservation)) {
+            throw  new Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now()
+        ]);
+    }
+
+    public function reservations()
+    {
+        return $this->hasMany(Reservation::class);
     }
 }
