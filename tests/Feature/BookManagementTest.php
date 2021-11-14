834408baa414561a78e2e695d747bc36fba5165d
Pavel Krokhmal <pavel.krokhmal.mn@outlook.com>
2021-11-14 14:11:46 +0200

    data() -> private

--- tests/Feature/BookManagementTest.php
+++ tests/Feature/BookManagementTest.php
@@ -92,7 +92,7 @@ class BookManagementTest extends TestCase
         $this->assertCount(1, Author::all());
     }
 
-    public function data()
+    private function data()
     {
         return [
             'title' => 'title',
